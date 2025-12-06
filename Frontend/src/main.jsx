fa789678f4bdd54832849adf7795b28be37c7ade
jimit8929 <jimit8929@gmail.com>
2025-12-06 23:58:24 +0530

    AuthContext.jsx

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -5,12 +5,15 @@ import App from "./App.jsx";
 
 import { BrowserRouter } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
+import { AuthProvider } from "./context/AuthContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-      <Toaster position="bottom-right" />
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <App />
+        <Toaster position="bottom-right" />
+      </BrowserRouter>
+    </AuthProvider>
   </StrictMode>
 );
