3ad6a7ecd87e06cc19ff8dfce279526f0b729486
jimit8929 <jimit8929@gmail.com>
2025-12-06 23:43:03 +0530

    Data.js and Helper.js

---
+++ Frontend/src/utils/data.js
@@ -0,0 +1,63 @@
+import { Lightbulb, BookOpen, Download, Library } from "lucide-react";
+
+export const FEATURES = [
+  {
+    title: "AI-Powered Writing",
+    description:
+      "Overcome writer's block with our smart assistant that helps you generate ideas, outlines, and content.",
+    icon: Lightbulb,
+    gradient: "from-violet-500 to-purple-600",
+  },
+  {
+    title: "Immersive Reader",
+    description:
+      "Preview your ebook in a clean, read-only format. Adjust font sizes for a comfortable reading experience",
+    icon: BookOpen,
+    gradient: "from-blue-500 to-cyan-600",
+  },
+  {
+    title: "One-Click Export",
+    description:
+      "Export your ebook to PDF, and DOCX formats instantly, ready for publishing",
+    icon: Download,
+    gradient: "from-emerald-500 to-teal-600",
+  },
+  {
+    title: "eBook Manangement",
+    description:
+      "Organize all your ebook projects in a personal dashboard, Easily track progress, edit drafts, and manage you ebook",
+    icon: Library,
+    gradient: "from-pink-500 to-rose-600",
+  },
+];
+
+export const TESTIMONIALS = [
+  {
+    quote:
+      "This platform made it so easy to write and publish my first ebook. The AI assistent is a game changer",
+    author: "Jane Doe",
+    title: "Bestselling Author",
+    avatar:
+      "https://plus.unsplash.com/premium_vector-1726141126614-9b761d1622b2?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGFpJTIwYXNzaXN0YW50fGVufDB8fDB8fHww",
+    rating: 5,
+  },
+
+  {
+    quote:
+      "I love the customizable templates.I was able to create a beautiful ebook that matches my brand perfectly.",
+    author: "John Smith",
+    title: "Marketing Expert",
+    avatar:
+      "https://plus.unsplash.com/premium_vector-1682310577739-32e6b98324f0?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8bWFya2V0aW5nJTIwZXhwZXJ0fGVufDB8fDB8fHww",
+    rating: 5,
+  },
+  {
+    quote:
+      "The one-click export feature saved me so much time. I was able to publish my ebook on multiple platforms",
+    author: "Peter Jones",
+    title: "Indie Publisher",
+    avatar:
+      "https://images.unsplash.com/photo-1575385151519-05e02c4f734f?q=80&w=1025&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    rating: 5,
+  },
+];
